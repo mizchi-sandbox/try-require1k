aae66ff23e3f21d770ed72bfcb9c22c24e8586a4
Koutarou Chikuba <miz404@gmail.com>
2015-01-30 14:48:06 +0900

    Init

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 var _ = require('lodash/index');
 var marked = require('marked/index');
+// var react = require('react/react');
+var react = require('react/dist/react.min');
 console.log(marked('# hello'));
